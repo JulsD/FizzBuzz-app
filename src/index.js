d19a08ce4d8d42e5acb5e811d1b47060fdb23850
JulsD <dutchak.julia@gmail.com>
2018-11-24 13:05:14 +0100

    task4: add error handlers

--- src/index.js
+++ src/index.js
@@ -11,23 +11,28 @@ const FizzBuzzApp = {
     },
 
     generateWord: function(opts) {
-        let {i, wordCreatorFn} = opts,
+        let _this = this,
+            {i, wordCreatorFn} = opts,
             word;
         switch (true) {
             case (i % 3) === 0 && (i % 5) === 0:
-                word = 'FizzBuzz';
+                word = _this.defaultWords.x3x5;
                 break;
 
             case (i % 3) === 0 && (i % 5) !== 0:
-                word = 'Fizz';
+                word = _this.defaultWords.x3;
                 break;
 
             case (i % 3) !== 0 && (i % 5) === 0:
-                word = 'Buzz';
+                word = _this.defaultWords.x5;
                 break;
 
             default:
-                word = wordCreatorFn({ withErrors: false });
+                try {
+                    word = wordCreatorFn({ withErrors: true });
+                } catch (e) {
+                    word = _this.defaultWords.x3.error;
+                }
                 break;
         }
         return word;
@@ -46,10 +51,9 @@ const FizzBuzzApp = {
         for(let i = 1; i <=n; i++) {
             let word = _this.generateWord({i, wordCreatorFn: getRandomWord});
             if(word.then) {
-                word.then(
-                    word => console.log(`${i}: ${word}`),
-                    error => console.log(`${i}: Something went wrong: ${error}`)
-                  );
+                word
+                .then( word => console.log(`${i}: ${word}`) )
+                .catch( error => console.log(`${i}: ${_this.errorWord}`) );
             } else {
                 console.log(`${i}: ${word}`);
             }
@@ -58,4 +62,4 @@ const FizzBuzzApp = {
 }
 
 FizzBuzzApp.printNumericItems(100);
-FizzBuzzApp.printNumericItemsAsync(16);
+FizzBuzzApp.printNumericItemsAsync(100);
