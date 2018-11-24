a50adbe3a3356b7ce65371b419888511aaae3171
JulsD <dutchak.julia@gmail.com>
2018-11-24 11:47:33 +0100

    task1: print numbered words

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const { getRandomWordSync, getRandomWord } = require('word-maker');
 
 console.log('It works!');
+printNumericItems(100);
 
-// YOUR CODE HERE
+function printNumericItems(n) {
+    for(let i = 1; i <=n; i++) {
+        const word = getRandomWordSync({ withErrors: false });
+        console.log(`${i}: ${word}`);
+    }
+}
