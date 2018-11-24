cd1cd7082c4b85e4205fbe5a9bcfd8b23f1393b5
JulsD <dutchak.julia@gmail.com>
2018-11-24 12:39:16 +0100

    task3: add reusable fn for word generation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ const { getRandomWordSync, getRandomWord } = require('word-maker');
 console.log('It works!');
 
 const FizzBuzzApp = {
+    generateWord: function(opts) {
+        let {i, wordCreatorFn} = opts,
+            word;
+        switch (true) {
+            case (i % 3) === 0 && (i % 5) === 0:
+                word = 'FizzBuzz';
+                break;
+
+            case (i % 3) === 0 && (i % 5) !== 0:
+                word = 'Fizz';
+                break;
+
+            case (i % 3) !== 0 && (i % 5) === 0:
+                word = 'Buzz';
+                break;
+
+            default:
+                word = wordCreatorFn({ withErrors: false });
+                break;
+        }
+        return word;
+    },
+
     printNumericItems: function(n) {
         let word;
         for(let i = 1; i <=n; i++) {
