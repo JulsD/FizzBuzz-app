52c9189717d362cc7847be26577e669798369565
JulsD <dutchak.julia@gmail.com>
2018-11-25 15:16:35 +0100

    move FizzBuzzApp to a separate module file

--- src/index.js
+++ src/index.js
@@ -1,68 +1,11 @@
-const { getRandomWordSync, getRandomWord } = require('word-maker');
+const FizzBuzzApp = require('./FizzBuzz');
 
-const FizzBuzzApp = {
-    defaultWords: {
-        x3: 'Fizz',
-        x5: 'Buzz',
-        x3x5: 'FizzBuzz',
-        error: 'Doh!'
-    },
+let results = {};
 
-    generateWord: function(opts) {
-        let _this = this,
-            {i, wordCreatorFn} = opts,
-            word;
-        try {
-            switch (true) {
-                case (i % 3) === 0 && (i % 5) === 0:
-                    word = _this.defaultWords.x3x5;
-                    break;
-
-                case (i % 3) === 0 && (i % 5) !== 0:
-                    word = _this.defaultWords.x3;
-                    break;
-
-                case (i % 3) !== 0 && (i % 5) === 0:
-                    word = _this.defaultWords.x5;
-                    break;
-
-                default:
-                    word = wordCreatorFn({ withErrors: true });
-                    break;
-            }
-        } catch (e) {
-            word = _this.defaultWords.error;
-        }
-        return word;
-    },
-
-    printNumericItems: function(n) {
-        let _this = this;
-        for(let i = 1; i <=n; i++) {
-            let word = _this.generateWord({i, wordCreatorFn: getRandomWordSync});
-            console.log(`${i}: ${word}`);
-        }
-    },
-
-    printNumericItemsAsync: function(n) {
-        let _this = this;
-        for(let i = 1; i <=n; i++) {
-            let word = _this.generateWord({i, wordCreatorFn: getRandomWord});
-            if(word.then) {
-                word
-                .then( word => console.log(`${i}: ${word}`) )
-                .catch( error => console.log(`${i}: ${_this.defaultWords.error}`) );
-            } else {
-                console.log(`${i}: ${word}`);
-            }
-        }
-    }
-}
+console.log('It works!');
 
 console.log('======= Synchronous =======');
 FizzBuzzApp.printNumericItems(100);
 
 console.log('======= Asynchronous =======');
 FizzBuzzApp.printNumericItemsAsync(100);
-
-console.log('It works!');
