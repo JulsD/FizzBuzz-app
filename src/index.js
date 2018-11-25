7e80ec016c71278373d2165572c9dfd71a883487
JulsD <dutchak.julia@gmail.com>
2018-11-25 19:09:14 +0100

    task5: write data to a file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const FizzBuzzApp = require('./FizzBuzz');
 const fetch = require('node-fetch');
+const fs = require('fs');
 
 console.log('It works!');
 
@@ -9,13 +10,14 @@ let dataSynchronous = FizzBuzzApp.printNumericItems(5),
 Promise.all([dataSynchronous, dataAsynchronous])
 .then(
     result => {
+        let jsonResult = JSON.stringify(result);
         fetch('https://httpbin.org/post', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(result)
+            body: jsonResult
         })
         .then(res => {
             console.log(res.ok);
@@ -23,6 +25,11 @@ Promise.all([dataSynchronous, dataAsynchronous])
             console.log(res.statusText);
         })
         .catch(err => console.error(err));
+
+        fs.writeFile('data.json', jsonResult, 'utf8', (err) => {
+            if (err) throw err;
+            console.log('The file has been created/updated!');
+        });
     },
     error => {
         console.log('Something goes wrong: ', error);
