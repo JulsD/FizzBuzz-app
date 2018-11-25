97bca78e4de2ded50d3f0aabae174d6a0e31fcab
JulsD <dutchak.julia@gmail.com>
2018-11-25 20:55:41 +0100

    add jest tests for FizzBuzz modul

---
+++ src/FizzBuzz.test.js
@@ -0,0 +1,110 @@
+const FizzBuzzApp = require('./FizzBuzz');
+const { getRandomWordSync, getRandomWord } = require('word-maker');
+const testWordGenerator = () => {
+    return 'World'
+}
+
+describe('generateWord function should:', () => {
+    test('for multiples of three, print "Fizz"', () => {
+        expect(FizzBuzzApp.generateWord({i:3, wordCreatorFn: testWordGenerator})).toBe('Fizz');
+    });
+    
+    test('for multiples of five, print "Buzz"', () => {
+        expect(FizzBuzzApp.generateWord({i:5, wordCreatorFn: testWordGenerator})).toBe('Buzz');
+    });
+    
+    test('for multiples of five or three, print "FizzBuzz"', () => {
+        expect(FizzBuzzApp.generateWord({i:15, wordCreatorFn: testWordGenerator})).toBe('FizzBuzz');
+    });
+    
+    test('if not multiples of five or three and no Error, print generated word', () => {
+        expect(FizzBuzzApp.generateWord({i:1, wordCreatorFn: testWordGenerator})).toBe('World');
+    });
+    
+    test('when an error is caught, print "Doh!"', () => {
+        expect(FizzBuzzApp.generateWord({i:2, wordCreatorFn: () => {throw Error}})).toBe('Doh!');
+    });
+    test('if not multiples of five or three and no Error, print generated word', () => {
+        const wordCreatorFnMock = jest.fn();
+        FizzBuzzApp.generateWord({i:1, wordCreatorFn: wordCreatorFnMock});
+        expect(wordCreatorFnMock).toHaveBeenCalled();
+    });
+});
+
+describe('printNumericItems function should:', () => {
+    let dataObj;
+
+    beforeAll(() => {
+        dataObj = FizzBuzzApp.printNumericItems(15);
+    });
+
+    afterAll(() => {
+        dataObj = null;
+    });
+
+    test('return N numbered list of words', () => {
+        expect(Object.keys(dataObj).length).toBe(15);
+    });
+
+    test('return "Fizz" word for the "3" key', () => {
+        expect(dataObj["3"]).toBe("Fizz");
+    });
+
+    test('return "Buzz" word for the "5" key', () => {
+        expect(dataObj["5"]).toBe("Buzz");
+    });
+
+    test('return "Buzz" word for the "15" key', () => {
+        expect(dataObj["15"]).toBe("FizzBuzz");
+    });
+});
+
+describe('printNumericItemsAsync function should:', () => {
+    let dataPromise;
+
+    beforeEach(() => {
+        dataPromise = FizzBuzzApp.printNumericItemsAsync(15);
+    });
+
+    afterEach(() => {
+        dataPromise = null;
+    });
+
+    test('return a Promise', () => {
+        dataPromise.then(result => {
+            expect(result.then).toBeTruthy();
+        });
+    });
+
+    test('return a Promise which returns N numbered list of words', () => {
+        dataPromise.then(promiseExp => {
+            promiseExp.then(data => {
+                expect(Object.keys(data).length).toBe(15);
+            })
+        });
+    });
+    
+    test('return "Fizz" word for the "3" key', () => {
+        dataPromise.then(promiseExp => {
+            promiseExp.then(data => {
+                expect(dataObj["3"]).toBe("Fizz");
+            })
+        });
+    });
+
+    test('return "Buzz" word for the "5" key', () => {
+        dataPromise.then(promiseExp => {
+            promiseExp.then(data => {
+                expect(dataObj["5"]).toBe("Buzz");
+            })
+        });
+    });
+
+    test('return "Buzz" word for the "15" key', () => {
+        dataPromise.then(promiseExp => {
+            promiseExp.then(data => {
+                expect(dataObj["15"]).toBe("FizzBuzz");
+            })
+        });
+    });
+});
